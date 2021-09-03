8e0a38b577cb988bbb1723614e7551f0f742631a
nntdatkh <nntdat.kh@gmail.com>
2021-09-03 20:20:25 +0700

    add collision detection for balls

--- main.js
+++ main.js
@@ -50,6 +50,20 @@ Ball.prototype.update = function() {
     this.y += this.velY;
   }
 
+  Ball.prototype.collisionDetect = function() {
+    for (let j = 0; j < balls.length; j++) {
+      if (!(this === balls[j])) {
+        const dx = this.x - balls[j].x;
+        const dy = this.y - balls[j].y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+  
+        if (distance < this.size + balls[j].size) {
+          balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) +')';
+        }
+      }
+    }
+  }
+
 let balls = [];
 
 while (balls.length < 25) {
@@ -75,6 +89,7 @@ function loop() {
     for (let i = 0; i < balls.length; i++) {
       balls[i].draw();
       balls[i].update();
+      balls[i].collisionDetect();
     }
     requestAnimationFrame(loop);
 }
